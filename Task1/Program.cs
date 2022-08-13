adba906ba46e06fb0bb46f12932b04f161e36258
Sarge <sarge2033@gmail.com>
2022-08-13 03:28:31 +0300

    работаю 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(num);
+
+if(number.Length == 5)
+{
+     if(number[0] == number[4] && number[1] == number[3])
+     {
+         Console.Write($"Ваше число {num} полиндром!");
+     }
+     else Console.Write($"Ваше число {num} не полиндром!");
+ }
+else Console.WriteLine("Введите правильное число!"); 
