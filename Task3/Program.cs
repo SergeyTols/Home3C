2bb07c8e1f0f819f37b1fc8439f296d6b8c71af0
Sarge <sarge2033@gmail.com>
2022-08-18 18:32:57 +0300

    работа 3

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+int b = 0;
+
+Console.Write("Кубы: ");
+
+while(a < N)
+{
+    b = Convert.ToInt32(Math.Pow(a, 3));
+    Console.Write(b + ", ");
+    a++;
+
+    if(a == N)
+    { 
+      b = Convert.ToInt32(Math.Pow(a, 3));
+      Console.Write(b);
+    }
+}    
