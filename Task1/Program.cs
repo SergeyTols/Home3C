78bb9e8d9397f9c4a054b80753ba46d8f44c9b4d
Sarge <sarge2033@gmail.com>
2022-08-18 18:36:59 +0300

    работа 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,8 +8,8 @@ if(number.Length == 5)
 {
      if(number[0] == number[4] && number[1] == number[3])
      {
-         Console.Write($"Ваше число {num} полиндром!");
+         Console.Write($"Ваше число {num} палиндром!");
      }
-     else Console.Write($"Ваше число {num} не полиндром!");
+     else Console.Write($"Ваше число {num} не палиндром!");
  }
 else Console.WriteLine("Введите правильное число!"); 
